a1d5eda758fb07b1c5aa28389c77e814ed5ced33
Nick Jordan <cnicholasjordan@gmail.com>
2016-02-16 08:05:50 -0800

    config db and routes for EC2

--- config/env/production.js
+++ config/env/production.js
@@ -1,16 +1,16 @@
-var port = 4567;
+var port = 5050;
 
 module.exports = {
   port: port,
-  db: 'mongodb://localhost:27017/pomodoro',
+  db: 'mongodb://NewUser:NewPwd@ec2-52-36-102-61.us-west-2.compute.amazonaws.com:27017/dummyDB',
   facebookAuth: {
     'clientID' : '765052613629038', 
     'clientSecret' : '54d866b02bf5788a99fbdb03c70477f1',
-    'callbackURL' : 'http://localhost:4567/auth/facebook/callback'
+    'callbackURL' : 'http://ec2-52-36-102-61.us-west-2.compute.amazonaws.com:5050/auth/facebook/callback'
   },
   googleAuth: {
     'clientID' : '164261624578-m2m29oj7p02fcgroa2vu5ogr2um2fav4.apps.googleusercontent.com',
     'clientSecret' : 'SdOsNupq3lQoGKHhPV1aFFca',
-    'callbackURL' : 'http://localhost:4567/auth/google/callback'
+    'callbackURL' : 'http://ec2-52-36-102-61.us-west-2.compute.amazonaws.com:5050/auth/google/callback'
   }
 }
